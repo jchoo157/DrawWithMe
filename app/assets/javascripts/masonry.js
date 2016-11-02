af407f898ee2ec9b9d05e6955b5f9979253fb499
Johnny Choo <jchoo156@gmail.com>
2016-11-02 12:38:51 -0700

    add color change to navbar and drawing div

---
+++ app/assets/javascripts/masonry.js
@@ -0,0 +1,7 @@
+$('#masonry-container').masonry({
+  itemSelector: '.box',
+  // set columnWidth a fraction of the container width
+  columnWidth: function( containerWidth ) {
+    return containerWidth / 5;
+  }
+});
