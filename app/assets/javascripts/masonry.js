4c41646e0672da42b95b095b1dac3e683787b88f
Johnny Choo <jchoo156@gmail.com>
2016-11-02 15:15:48 -0700

    remove masonry from app

--- app/assets/javascripts/masonry.js
+++
@@ -1,7 +0,0 @@
-$('#masonry-container').masonry({
-  itemSelector: '.box',
-  // set columnWidth a fraction of the container width
-  columnWidth: function( containerWidth ) {
-    return containerWidth / 5;
-  }
-});
